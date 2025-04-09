8bbe257b5cc455fe45267a1ead47f6dbd2bdafc0
Paulo Bueno <paulophmercham23@gmail.com>
2025-04-09 19:35:56 -0300

    Adiciona menu dinâmico e página sobre mim

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleTexto(botao) {
+    const card = botao.closest('.Card');
+    const textoExtra = card.querySelector('.conteudo-completo');
+  
+    textoExtra.classList.toggle('expandido');
+  
+    if (textoExtra.classList.contains('expandido')) {
+      botao.textContent = "Mostrar menos";
+    } else {
+      botao.textContent = "Mostrar mais";
+    }
+  }
+
+function toggleMenu() {
+  console.log("Cliquei no menu!");
+  const menu = document.getElementById('menu-list');
+  menu.classList.toggle('show');
+}
+
+fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+  });
